3ee1fb05fbd03bc7a05b2b7b168330b036e1a15c
Sajjan Choudhary <148364962+sajjan420@users.noreply.github.com>
2025-06-27 15:01:03 +0530

    Add files via upload

---
+++ Landing-Page/script.js
@@ -0,0 +1,7 @@
+function handleStart() {
+  alert("Welcome! We'll help you secure your digital assets.");
+}
+
+function handleDemo() {
+  alert("Demo request submitted! Our team will contact you soon.");
+}
